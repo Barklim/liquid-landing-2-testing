54dc59028bf799575cf0f2b98bbc15ce22675008
Barklim <46284548+Barklim@users.noreply.github.com>
2021-10-28 14:27:43 +0800

    fix modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -18,13 +18,7 @@ import { useForm } from "react-hook-form";
 import PhoneInput from "react-phone-number-input";
 import PdfModal from "../../components/Pdf";
 
-import {
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-  Box,
-} from "@chakra-ui/react";
+import { Alert, AlertIcon, AlertTitle, Box } from "@chakra-ui/react";
 
 import Config from "../../utils/config";
 
@@ -36,6 +30,8 @@ function ModalHomePage() {
   const [value, setValue] = useState("");
 
   const [alertProp, setAlert] = useState("lq__alert");
+
+  const [submited, setSubmited] = useState(false);
 
   const {
     register,
@@ -63,14 +59,13 @@ function ModalHomePage() {
     // }, 2000);
 
     setAlert("lq__alert displayVisisble");
+    setSubmited(true);
 
     if (Config.isProd) {
       Config.isTesting
         ? window.ym(85672684, "reachGoal", "sendtoapp")
         : window.ym(85672684, "reachGoal", "send");
     }
-
-    console.log("WTF");
   };
 
   return (
@@ -95,161 +90,186 @@ function ModalHomePage() {
         <ModalOverlay />
         <ModalContent onSubmit={handleSubmit(onSubmit)}>
           <form>
-            <ModalHeader className="label">Получить кредит</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody
-              pb={6}
-              className={
-                Config.isTesting ? "modal__body_testing" : "modal__body"
-              }
-            >
-              <div className={alertProp}>
-                <Alert status="success">
-                  <AlertIcon />
-                  <Box flex="1">
-                    <AlertTitle>Успешно отправлено!</AlertTitle>
-                    {Config.isTesting ? (
-                      <AlertDescription display="block">
-                        Cпасибо за заявку. Приложение в разработке, мы отправим
-                        вам SMS когда приложение будет готово.
-                      </AlertDescription>
-                    ) : (
-                      <AlertDescription display="block">
-                        Мы рассмотрим вашу заявку и ответим в течение следующих
-                        48 часов.
-                      </AlertDescription>
-                    )}
-                  </Box>
-                </Alert>
-              </div>
-              {!Config.isTesting && (
-                <FormControl className="modal__formControl">
-                  <FormLabel>
-                    Имя <span className="label__star">*</span>
-                  </FormLabel>
-                  <Input
-                    // ref={initialRef}
-                    placeholder="Иван"
-                    name="name"
-                    ref={register({
-                      required: true,
-                      minLength: {
-                        value: 1,
-                        message: "Введите имя",
-                      },
-                    })}
-                  />
-                  {errors.name?.type === "required" && (
-                    <div className="error">Введите имя</div>
-                  )}
-                </FormControl>
-              )}
-
-              <FormControl className="modal__formControl">
-                <FormLabel>
-                  Телефон <span className="label__star">*</span>
-                </FormLabel>
-                <PhoneInput
-                  defaultCountry="RU"
-                  placeholder="+7 (_ _ _) _ _ _ - _ _ - _ _"
-                  value={value}
-                  onChange={setValue}
-                  className="PhoneInputInput"
-                  name="tel"
-                  control={control}
-                  ref={register({
-                    required: true,
-                    minLength: {
-                      value: 1,
-                      message: "Введите номер",
-                    },
-                  })}
-                />
-                {value?.length > 12 && value?.length !== 0 && (
-                  <div className="error">Вы ввели слишком много цифр</div>
-                )}
-                {value?.length < 12 &&
-                  value?.length !== 0 &&
-                  value?.length !== 2 && (
-                    <div className="error">
-                      Введите корректный номер телефона
-                    </div>
-                  )}
-                {errors.tel?.type === "required" && (
-                  <div className="error">Введите номер телефона</div>
-                )}
-              </FormControl>
-
-              {!Config.isTesting && (
-                <FormControl mt={4} className="modal__formControl">
-                  <FormLabel>Почта</FormLabel>
-                  <Input
-                    placeholder="example@gmail.com"
-                    ref={register({
-                      required: false,
-                      // pattern: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g,
-                    })}
-                    name="email"
-                  />
-                  {errors.email?.type === "required" && (
-                    <div className="error">Введите почту</div>
-                  )}
-                </FormControl>
-              )}
-
-              <div className="modal__agreement">
-                <label className="form__customCheckbox">
-                  <input
-                    type="checkbox"
-                    className="form__checkbox"
-                    name="agreement"
-                    ref={register({
-                      required: true,
-                    })}
-                  ></input>
-                  <span></span>
-                </label>
-                <PdfModal>
-                  <div className="modal__agreementTextWrap">
-                    Соглашаюсь{" "}
-                    <span className="modal__agreementText">
-                      с правилами обработки&nbsp;
-                      {/* <br /> */}
-                      персональных данных
-                    </span>
-                  </div>
-                </PdfModal>
-              </div>
-              {errors.agreement?.type === "required" && (
-                <div className="error">Примите соглашение</div>
-              )}
-
-              {Config.isTesting && (
-                <div className="modal__testingText">
-                  На&nbsp;этот&nbsp;номер&nbsp;мы&nbsp;отправим&nbsp;вам
-                  <br />
-                  ссылку&nbsp;на&nbsp;приложение
+            {!Config.isTesting && (
+              <ModalHeader className="label">Получить кредит</ModalHeader>
+            )}
+            {Config.isTesting && !submited && (
+              <>
+                <div className="modal__testingText modal__testingText_Desktop">
+                  {/* На&nbsp;этот&nbsp;номер&nbsp;мы&nbsp;отправим&nbsp;вам
+                <br />
+                ссылку&nbsp;на&nbsp;приложение */}
+                  На этот номер мы отправим вам ссылку&nbsp;на&nbsp;приложение
                 </div>
-              )}
-            </ModalBody>
+                <div className="modal__testingText modal__testingText_Mobile">
+                  На&nbsp;этот&nbsp;номер&nbsp;мы&nbsp;отправим
+                  вам&nbsp;ссылку&nbsp;на&nbsp;приложение
+                </div>
+              </>
+            )}
+            <ModalCloseButton />
+            {!submited && (
+              <>
+                <ModalBody
+                  pb={6}
+                  className={
+                    Config.isTesting ? "modal__body_testing" : "modal__body"
+                  }
+                >
+                  <div className={alertProp}>
+                    <Alert status="success">
+                      <AlertIcon />
+                      <Box flex="1">
+                        <AlertTitle>Успешно отправлено!</AlertTitle>
+                      </Box>
+                    </Alert>
+                  </div>
+                  {!Config.isTesting && (
+                    <FormControl className="modal__formControl">
+                      <FormLabel>
+                        Имя <span className="label__star">*</span>
+                      </FormLabel>
+                      <Input
+                        // ref={initialRef}
+                        placeholder="Иван"
+                        name="name"
+                        ref={register({
+                          required: true,
+                          minLength: {
+                            value: 1,
+                            message: "Введите имя",
+                          },
+                        })}
+                      />
+                      {errors.name?.type === "required" && (
+                        <div className="error">Введите имя</div>
+                      )}
+                    </FormControl>
+                  )}
 
-            <ModalFooter className="modal__footer">
-              <Button
-                type="submit"
-                className="button_primary1"
-                colorScheme="blue"
-                mr={3}
-              >
-                Отправить
-              </Button>
+                  <FormControl className="modal__formControl">
+                    <FormLabel>
+                      Телефон <span className="label__star">*</span>
+                    </FormLabel>
+                    <PhoneInput
+                      defaultCountry="RU"
+                      placeholder="+7 (_ _ _) _ _ _ - _ _ - _ _"
+                      value={value}
+                      onChange={setValue}
+                      className="PhoneInputInput"
+                      name="tel"
+                      control={control}
+                      ref={register({
+                        required: true,
+                        minLength: {
+                          value: 1,
+                          message: "Введите номер",
+                        },
+                      })}
+                    />
+                    {value?.length > 12 && value?.length !== 0 && (
+                      <div className="error">Вы ввели слишком много цифр</div>
+                    )}
+                    {value?.length < 12 &&
+                      value?.length !== 0 &&
+                      value?.length !== 2 && (
+                        <div className="error">
+                          Введите корректный номер телефона
+                        </div>
+                      )}
+                    {errors.tel?.type === "required" && (
+                      <div className="error">Введите номер телефона</div>
+                    )}
+                  </FormControl>
 
-              <Button
-                className="button_primary1 button_close"
-                onClick={onClose}
-              >
-                Закрыть
-              </Button>
-            </ModalFooter>
+                  {!Config.isTesting && (
+                    <FormControl mt={4} className="modal__formControl">
+                      <FormLabel>Почта</FormLabel>
+                      <Input
+                        placeholder="example@gmail.com"
+                        ref={register({
+                          required: false,
+                          // pattern: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g,
+                        })}
+                        name="email"
+                      />
+                      {errors.email?.type === "required" && (
+                        <div className="error">Введите почту</div>
+                      )}
+                    </FormControl>
+                  )}
+
+                  <div className="modal__agreement">
+                    <label className="form__customCheckbox">
+                      <input
+                        type="checkbox"
+                        className="form__checkbox"
+                        name="agreement"
+                        ref={register({
+                          required: true,
+                        })}
+                      ></input>
+                      <span></span>
+                    </label>
+                    <PdfModal>
+                      <div className="modal__agreementTextWrap">
+                        Соглашаюсь{" "}
+                        <span className="modal__agreementText">
+                          с правилами обработки&nbsp;
+                          {/* <br /> */}
+                          персональных данных
+                        </span>
+                      </div>
+                    </PdfModal>
+                  </div>
+                  {errors.agreement?.type === "required" && (
+                    <div className="error">Примите соглашение</div>
+                  )}
+                </ModalBody>
+
+                <ModalFooter
+                  className={
+                    Config.isTesting
+                      ? "modal__footer modal__footer_testing"
+                      : "modal__footer"
+                  }
+                >
+                  <Button
+                    type="submit"
+                    className="button_primary1"
+                    colorScheme="blue"
+                    mr={3}
+                  >
+                    Отправить
+                  </Button>
+
+                  <Button
+                    className="button_primary1 button_close"
+                    onClick={onClose}
+                  >
+                    Закрыть
+                  </Button>
+                </ModalFooter>
+              </>
+            )}
+            {submited && (
+              <ModalBody pb={6} className="successMessage">
+                {Config.isTesting ? (
+                  <span>
+                    Спасибо за заявку 🎉 <br />
+                    <br /> Приложение в разработке, <br /> мы отправим вам SMS
+                    когда приложение будет готово.
+                  </span>
+                ) : (
+                  <span>
+                    Успешно отправлено 🎉 <br />
+                    <br />
+                    Мы рассмотрим вашу заявку и ответим в течение следующих 48
+                    часов.
+                  </span>
+                )}
+              </ModalBody>
+            )}
           </form>
         </ModalContent>
       </Modal>
